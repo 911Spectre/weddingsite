445f6bfb2bd1ec408f6b7bc87dcf9ff00472acc5
Erjan Osmonov <2007erjan2007@gmail.com>
2025-02-21 20:41:57 +0600

    added guestList, form functionality, tweaked design a bit

--- script.js
+++ script.js
@@ -41,5 +41,64 @@ function initMap() {
     .openPopup();
 }
 
+// ! Добавление человека в список гостей
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.getElementById('registrationForm');
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const name = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
+    const phone = document.getElementById('phone').value;
+    
+    // Получение предыдущего объекта с гостями из sessionStorage
+    let people = JSON.parse(sessionStorage.getItem('people')) || [];
+    
+    // Новый гость
+    let newPerson = {
+      name: name,
+      lastName: lastName,
+      phone: phone,
+    }
+    
+    // Добавление человека в список гостей
+    people.push(newPerson);
+    
+    // Отправка данных в sessionStorage
+    sessionStorage.setItem('people', JSON.stringify(people))
+
+    form.reset();
+
+    alert("You've been listed!")
+  })
+})
+
+// Отображение списка гостей 
+function listGuests () {
+  const peopleData = sessionStorage.getItem('people');
+
+  if (peopleData) {
+    const people = JSON.parse(peopleData);
+
+    const peopleList = document.getElementById('guestList__items');
+
+    peopleList.innerHTML = '';
+
+    people.map(person => {
+      const li = document.createElement('li');
+      li.textContent = `${person.lastName} ${person.name}`;
+      peopleList.append(li);
+    })
+  } else {
+    const li = document.createElement('li');
+    li.textContent = "No guests";
+    document.getElementById('guestList__items').append(li);
+  }
+}
+
+listGuests();
+
+
 // Загрузка карты после загрузки DOM
 document.addEventListener('DOMContentLoaded', initMap);
